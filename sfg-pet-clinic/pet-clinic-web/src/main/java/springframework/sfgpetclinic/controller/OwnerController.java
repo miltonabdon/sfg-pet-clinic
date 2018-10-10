d79f534e901cd4f6a970cc2de7afbf38927c9121
Milton Abdon do Nascimento Junior <miltonabdon69@gmail.com>
2018-10-09 22:44:50 -0300

    Added owner list and updated controller. Closes #16.

--- sfg-pet-clinic/pet-clinic-web/src/main/java/springframework/sfgpetclinic/controller/OwnerController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,14 +1,27 @@
 package springframework.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import springframework.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 	
+	//usando final determina que objeto nao tera mudanças e spring injeta depencia
+	private final OwnerService ownerService;
+	
+	//usando construtor, nao preciso utilizar o @autowired, #melhorpratica
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"","/", "/index", "/index.html"})
-	public String listOwners() {
+	public String listOwners(Model model) {
+		
+		model.addAttribute("owners",ownerService.findAll());
 		
 		return "owners/index";	
 		
