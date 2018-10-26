390875cbed0dcf88d256f6dcbe28b8e4eeafe73b
Milton Abdon do Nascimento Junior <miltonabdon69@gmail.com>
2018-10-26 15:12:12 -0300

    added wro4j plugin and config from Spring Pet Clinic. Closes #24.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/springframework/sfgpetclinic/services/OwnerService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/springframework/sfgpetclinic/services/OwnerService.java
@@ -1,5 +1,7 @@
 package springframework.sfgpetclinic.services;
 
+import org.springframework.stereotype.Service;
+
 import springframework.sfgpetclinic.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long>{
--- sfg-pet-clinic/pet-clinic-web/src/test/java/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -3,7 +3,10 @@ package springframework.sfgpetclinic;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import springframework.sfgpetclinic.bootstrap.DataLoader;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,7 @@ public class SfgPetClinicApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		System.out.println("entrou");
 	}
 
 }
